9604e4608d0342f2a023f646df6c45e75fb900ca
Carolina Falado <togykip@gmail.com>
2019-09-03 20:06:27 +1200

    js functions

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
-// Why pay a fortune teller when you can just program your fortune yourself ?
-
-//     Write a function named tellFortune that:
-// takes 4 arguments: number of children, partner's name, geographic location, job title.
-// outputs your fortune to the screen like so: "You will be a X in Y, and married to Z with N kids."
-// Call that function 3 times with 3 different values for the arguments.
-
-function tellFortune(numberOfChildren, partnerName, geoLocation, job) {
-    var fortune = ("You will be a " + job + "in " + geoLocation + ',' + "and married to " + partnerName + "with " + numberOfChildren
-        + 'kids')
-    alert(fortune);
+function calculateDogAge(agePuppy) {
+    var ageConversion = (agePuppy * 7);
+    console.log("your puppy is " + ageConversion + " years old in dog years");
 }
-
-var numberOfChildren = prompt("pick a number from 1-4?");
-var partnerName = prompt("Tell me your partner's name");
-var geoLocation = prompt("where do you live");
-var job = prompt("what do you want to be when you're older?");
-console.log(tellFortune(numberOfChildren, partnerName, geoLocation, job));
+console.log(calculateDogAge(prompt("enter your dog's age in human years")));
