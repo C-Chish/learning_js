3d8abd60fab6b2c8775cc7856bcb3b3eb7737729
Carolina Falado <togykip@gmail.com>
2019-09-09 19:45:29 +1200

    arrays

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
-function calculateDogAge(agePuppy) {
-    var ageConversion = (agePuppy * 7);
-    console.log("your puppy is " + ageConversion + " years old in dog years");
-}
-console.log(calculateDogAge(prompt("enter your dog's age in human years")));
+//Arrays
+
+/*var names = ['lol','cake','egg'];
+var years = new Array(1990, 1999, 1929);
+
+names[1] = 'ben';
+names[names.length] = "mary";
+console.log(names);
+*/
+
+/*
+
+//functions are called methods
+ lina  = ['red', 'leslie', 25, false];
+ //.push add to the last array (appends)
+ lina.push('cake');
+ //.unshift insert new elements into the start of an array
+ lina.unshift('Mrs');
+ //.pop removes the last element of an array and returns it
+ lina.pop()
+ //.shift removes the first element of an array and returns it
+ lina.shift();
+ console.log(lina);
+//.indexOf returns the position of the value in the array
+console.log(lina.indexOf('leslie')); 
+
+var isSkinny = lina.indexOf('skinny') === -1 ? 'lina is fat' : 'lina is skinny';
+console.log(isSkinny);
+*/
+
